c2d98764be83812a310bd041216c4e11e97a98f3
Your Name <you@example.com>
2025-03-20 22:51:29 +0600

    creating an text animation where text is coming outside of view

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ gsap.to('#box',{
     delay:1,
     backgroundColor:'red',
     duration:2.5,
+    repeat:-1,
+    yoyo:true,    
+})
+gsap.from('#text h2', {
+    opacity:0,
+    x:-100,
+    // y:30,
+    duration:1,
+    delay:0.5,
+    stagger:0.5,
 })
