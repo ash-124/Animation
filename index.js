c57f1578bf99d4100c96bed3043e004a0854425f
Your Name <you@example.com>
2025-03-20 23:21:27 +0600

    Created animation using timeline method of gsap

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const tl = gsap.timeline();
+
 gsap.to('#box',{
     x:700,
     borderRadius:'50%',
@@ -6,13 +8,43 @@ gsap.to('#box',{
     backgroundColor:'red',
     duration:2.5,
     repeat:-1,
-    yoyo:true,    
+    yoyo:true, 
+    ease:'slow'   
 })
 gsap.from('#text h2', {
     opacity:0,
     x:-100,
-    // y:30,
-    duration:1,
+    y:30,
+    duration:2,
     delay:0.5,
+    // repeatDelay:1,
     stagger:0.5,
+    repeat:-1,
+    yoyo:true,
+})
+
+tl.to('.box1',{
+    x:500,
+    delay:0.5,
+    duration:2,
+    repeat:-1,
+    yoyo:true,
+    rotate:360,
+    marginTop:'20px'
+})
+tl.to('.box2',{
+    x:600,
+    delay:0.5,
+    duration:2,
+    repeat:-1,
+    yoyo:true,
+    rotate:360,
+})
+tl.to('.box3',{
+    x:700,
+    delay:0.5,
+    duration:2,
+    repeat:-1,
+    yoyo:true,
+    rotate:360,
 })
