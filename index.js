9569309dfe5037be9fe5f17e10dbbddbcfca1add
Your Name <you@example.com>
2025-03-21 12:25:28 +0600

    Basic use of scrollTrigger Practice done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const tl = gsap.timeline();
 
 gsap.to('#box',{
-    x:700,
+    x:200,
     borderRadius:'50%',
     rotate:360,
     delay:1,
@@ -22,29 +22,60 @@ gsap.from('#text h2', {
     repeat:-1,
     yoyo:true,
 })
+// animations with gsap timeline
+// tl.to('.box1',{
+//     x:500,
+//     delay:0.5,
+//     duration:2,
+//     repeat:-1,
+//     yoyo:true,
+//     rotate:360,
+//     marginTop:'20px'
+// })
+// tl.to('.box2',{
+//     x:600,
+//     delay:0.5,
+//     duration:2,
+//     repeat:-1,
+//     yoyo:true,
+//     rotate:360,
+// })
+// tl.to('.box3',{
+//     x:700,
+//     delay:0.5,
+//     duration:2,
+//     repeat:-1,
+//     yoyo:true,
+//     rotate:360,
+// })
 
-tl.to('.box1',{
-    x:500,
-    delay:0.5,
-    duration:2,
-    repeat:-1,
-    yoyo:true,
-    rotate:360,
-    marginTop:'20px'
+// animation with scrolltrigger;
+
+gsap.from('#page2 .box2',{
+    scale:0,
+    rotate:-360,
+    duration:1,
+    scrollTrigger:{
+        trigger:'#page2 .box2',
+        scroller:'body',
+        markers:true,
+        start:'top 50%',
+        end:'top 40%',
+        scrub:4,
+        pin:true
+    }
 })
-tl.to('.box2',{
-    x:600,
-    delay:0.5,
-    duration:2,
-    repeat:-1,
-    yoyo:true,
-    rotate:360,
+gsap.from('#page3 .box3',{
+    scale:0,
+    rotate:-360,
+    duration:1,
+    delay:1,
+    
 })
-tl.to('.box3',{
-    x:700,
-    delay:0.5,
-    duration:2,
-    repeat:-1,
-    yoyo:true,
-    rotate:360,
+gsap.from('#page4 .box4',{
+    scale:0,
+    rotate:-360,
+    duration:1,
+    delay:1
 })
+
