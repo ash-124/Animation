18e0826a2f3e1ab4af5f3eb4ecd64e848ebfaa78
Your Name <you@example.com>
2025-03-22 23:17:30 +0600

    install latest tailwind css and done basic motion animation

--- learning-motion/src/App.jsx
+++ learning-motion/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { motion } from "motion/react"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-gray-800 w-full min-h-screen" >
+      <motion.div
+      className="w-36 h-36 bg-teal-500 rounded-xl "
+      initial={{
+        x:-100,
+        opacity:0
+      }}
+      
+      animate={
+       { 
+        x:800,
+        opacity:1
+      }
+      }
+      transition={
+        {
+          duration:2,
+          delay:1,
+          ease:'anticipate'
+        }
+      }
+      
+      ></motion.div>
+    </div>
   )
 }
 
